838c58ecf036496b53742faca0f8dbe33f86cd7a
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-01-30 08:53:02 +0600

    mongodb conected and 1 colliction ready

--- index.js
+++ index.js
@@ -8,14 +8,51 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-// medillwares
-
+// medillwares It has to do with allowing other domains to make requests against web API.
 app.use(cors());
 app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.POWERRANK_CLIENT}:${process.env.POWERRANK_PASSWORD}@cluster0.kbyx5ha.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+  try {
+    const billCollection = client.db('powerhack').collection('bills');
+
+
+
+    app.get('/billing-list', async (req, res) => {
+      const query = {};
+      const cursor = billCollection.find(query);
+      const product = await cursor.toArray()
+      res.send(product)
+    })
+
+    app.post('/add-billing', async (req, res) => {
+      const bill = req.body;
+      const result = await billCollection.insertOne(bill);
+      res.send(result)
+    })
+
+
+
+
+  }
+  finally {
+
+  }
+
+}
+run().catch(error => console.log(error))
+
+
+
+
+// Just for testing perpous
 app.get('/', (req, res) => {
   res.send('server runing')
 })
